a0f3b58d61c4ea775b9a437be4d1e864784cc7d1
sohan <sohanur>
2018-10-05 12:28:36 +0600

    added

---
+++ led_multiple/led_multiple.ino
@@ -0,0 +1,40 @@
+void setup(){
+
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(9,  OUTPUT);
+}
+
+
+void loop(){
+
+  digitalWrite(13, HIGH);
+  delay(500);
+
+  digitalWrite(13, LOW);
+
+
+  digitalWrite(12, HIGH);
+  delay(500);
+
+   digitalWrite(12, LOW);  
+
+
+  digitalWrite(11, HIGH);
+  delay(500);
+
+  digitalWrite(11, LOW);
+
+
+  digitalWrite(10, HIGH);
+  delay(500);
+  digitalWrite(10, LOW);
+
+   
+  digitalWrite(9, HIGH);
+  delay(500);
+  digitalWrite(9, LOW);
+
+}
